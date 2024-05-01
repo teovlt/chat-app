5bee8383386925d9681c0b02242615ea7780de6d
villett <Teo.Villet@gipsa-lab.grenoble-inp.fr>
2024-05-02 00:24:00 +0200

    Add message and extractTime function in utils that takes createdAt paramterers (mongoDB attribute) and return the times in hour and minutes Add message skeleton for when the conversation is loading the messages Add scroll to the bottom of the conversation  everytime we load messages or send a new one

--- client/src/components/messages/Message.jsx
+++ client/src/components/messages/Message.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
+import { useAuthContext } from '../../context/authContext.jsx'
+import useConversation from '../../store/useConversation'
+import { extractTime } from '../../utils/extractTime.js'
 
-function Message() {
+const Message = ({ message }) => {
+  const { authUser } = useAuthContext()
+  const { selectedConversation } = useConversation()
+  const fromMe = message.senderId === authUser._id
+  const chatClassNames = fromMe ? 'chat chat-end' : ' chat chat-start'
+  const profilePicture = fromMe ? authUser.profilePicture : selectedConversation?.profilePicture
+  const bubbleColor = fromMe ? 'bg-blue-500' : 'bg-gray-500'
+
   return (
-    <div className='chat chat-end'>
+    <div className={chatClassNames}>
       <div className='chat-image avatar'>
         <div className='w-10 rounded-full'>
-          <img alt='Tailwind CSS chat bubble component' src='https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg' />
+          <img alt='Tailwind CSS chat bubble component' src={profilePicture} />
         </div>
       </div>
-      <div className={`chat-bubble text-white bg-blue-500`}>Hi! What is upp?</div>
-      <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12:42</div>
+      <div className={`chat-bubble text-white ${bubbleColor} pb-2`}>{message.message}</div>
+      <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{extractTime(message.createdAt)}</div>
     </div>
   )
 }
--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Message from './Message'
+import useGetMessages from '../../hooks/useGetMessages'
+import MessageSkeleton from '../skeletons/MessageSkeleton'
 
-function Messages() {
+const Messages = () => {
+  const { messages, loading } = useGetMessages()
+  const lastMessageRef = useRef()
+
+  useEffect(() => {
+    setTimeout(() => lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' }), 100)
+  }, [messages])
+
   return (
     <div className='px-4 flex-1 overflow-auto '>
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <div ref={lastMessageRef} key={message._id}>
+            <Message message={message} />
+          </div>
+        ))}
+      {loading && [...Array(4)].map((_, idx) => <MessageSkeleton key={idx} />)}{' '}
+      {!loading && messages.length === 0 && (
+        <p className='text-center text-white'>Send a message to start the conversation</p>
+      )}
     </div>
   )
 }

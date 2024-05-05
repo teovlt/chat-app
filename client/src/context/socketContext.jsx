fa355b72a2013fc2bac59058d1100c2a3fdce44e
Téo <teo.villet2@gmail.com>
2024-05-06 00:25:16 +0200

    Add url to use socket when deploying the app Trying to change the name of the tab

--- client/src/context/socketContext.jsx
+++ client/src/context/socketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io('http://localhost:8000', {
+      const socket = io('https://chat-app-production-905w.onrender.com', {
         query: {
           userId: authUser._id,
         },

91ff21335c62ad71214f66fb83116f9baa39dbd6
Téo <teo.villet2@gmail.com>
2024-05-05 20:24:56 +0200

    Add deployement parameters and scripts

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import Express, { urlencoded } from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 
 import authRoutes from './routes/authRoutes.js'
 import messageRoutes from './routes/messageRoutes.js'
@@ -15,6 +16,8 @@ dotenv.config()
 // Server
 const PORT = process.env.PORT
 
+const __dirname = path.resolve()
+
 app.use(Express.json()) //to parse the incoming requests with JSON payloads
 app.use(cookieParser())
 
@@ -24,6 +27,12 @@ app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 // Users routes
 app.use('/api/users', userRoutes)
+
+app.use(Express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/dist/index.html'))
+})
 
 server.listen(PORT, () => {
   connectToDatabase()
